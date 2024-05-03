268112a3370b5744fe7da3c91d24ebfc9de0fa9e
mickael thumerel <mickael.thumerel@elvexoft.com>
2024-05-03 15:31:16 +0200

    rollback to recort typ with datacontract removed

--- src/Data.Block.Abstractions/BlockPoint.cs
+++ src/Data.Block.Abstractions/BlockPoint.cs
@@ -6,73 +6,89 @@ namespace Data.Block.Abstractions
 {
     using System.ComponentModel;
     using System.Diagnostics;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Runtime.Serialization;
 
     /// <summary>
     /// Define a point in user space
     /// </summary>
-    [DataContract]
+    //[DataContract]
     [Serializable]
     [ImmutableObject(true)]
     [DebuggerDisplay("({X}, {Y})")]
-    public readonly struct BlockPoint
+    public record struct BlockPoint(float X, float Y)
     {
-        #region Fields
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BlockPoint"/> struct.
-        /// </summary>
-        public BlockPoint(float x, float y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Gets the x.
-        /// </summary>
-        [DataMember]
-        public float X { get; }
-
-        /// <summary>
-        /// Gets the y.
-        /// </summary>
-        [DataMember] 
-        public float Y { get; }
-
-        #endregion
-
-        #region Methods
-
         /// <inheritdoc />
-        public static BlockPoint operator+(BlockPoint source, BlockPoint other)
+        public static BlockPoint operator +(BlockPoint source, BlockPoint other)
         {
             return new BlockPoint(source.X + other.X, source.Y + other.Y);
         }
-
-        /// <inheritdoc />
-        public override bool Equals([NotNullWhen(true)] object? obj)
-        {
-            if (obj is BlockPoint otherBP)
-            {
-                return this.X == otherBP.X &&
-                       this.Y == otherBP.Y; 
-            }
-
-            return false;
-        }
-
-        /// <inheritdoc />
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(this.X, this.Y);
-        }
-
-        #endregion
     }
+
+    //    /// <summary>
+    //    /// Define a point in user space
+    //    /// </summary>
+    //    [DataContract]
+    //    [Serializable]
+    //    [ImmutableObject(true)]
+    //    [DebuggerDisplay("({X}, {Y})")]
+    //#pragma warning disable IDE0250 // Make struct 'readonly' System.Text.Json failed to deserialize readonly struct
+    //    public struct BlockPoint
+    //#pragma warning restore IDE0250 // Make struct 'readonly'
+    //    {
+    //        #region Fields
+
+    //        /// <summary>
+    //        /// Initializes a new instance of the <see cref="BlockPoint"/> struct.
+    //        /// </summary>
+    //        public BlockPoint(float x, float y)
+    //        {
+    //            this.X = x;
+    //            this.Y = y;
+    //        }
+
+    //        #endregion
+
+    //        #region Properties
+
+    //        /// <summary>
+    //        /// Gets the x.
+    //        /// </summary>
+    //        [DataMember]
+    //        public float X { get; }
+
+    //        /// <summary>
+    //        /// Gets the y.
+    //        /// </summary>
+    //        [DataMember] 
+    //        public float Y { get; }
+
+    //        #endregion
+
+    //        #region Methods
+
+    //        /// <inheritdoc />
+    //        public static BlockPoint operator+(BlockPoint source, BlockPoint other)
+    //        {
+    //            return new BlockPoint(source.X + other.X, source.Y + other.Y);
+    //        }
+
+    //        /// <inheritdoc />
+    //        public override bool Equals([NotNullWhen(true)] object? obj)
+    //        {
+    //            if (obj is BlockPoint otherBP)
+    //            {
+    //                return this.X == otherBP.X &&
+    //                       this.Y == otherBP.Y; 
+    //            }
+
+    //            return false;
+    //        }
+
+    //        /// <inheritdoc />
+    //        public override int GetHashCode()
+    //        {
+    //            return HashCode.Combine(this.X, this.Y);
+    //        }
+
+    //        #endregion
+    //    }
 }
